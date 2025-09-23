78558460abe30a6268e3e330b99bd1d39b2265ea
LexsHubNew <albibot69@gmail.com>
2025-09-23 20:01:12 +0700

    Update and rename Main to Main.lua

---
+++ Loader/Main.lua
@@ -0,0 +1,61 @@
+local placeId = game.PlaceId
+local StarterGui = game:GetService("StarterGui")
+local gameName, success = nil, false
+
+if placeId == 2753915549 then
+    gameName = "Blox Fruit"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-a-script/main.lua"))()
+    success = true
+elseif placeId == 79546208627805 then
+    gameName = "99 Night In The Forest"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Loader/GrowAGarden.lua"))()
+    success = true
+elseif placeId == 18687417158 then
+    gameName = "Forsaken"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/create-stree/STREE-HUB/refs/heads/main/Forsaken/Main.lua"))()
+    success = true
+elseif placeId == 121864768012064 then
+    gameName = "Fish It"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/albibot69-lgtm/Lexs/refs/heads/main/Fish_It/Main.lua"))() 
+    success = true
+elseif placeId == 123921593837160 then
+    gameName = "Climb and Jump tower"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-5/main.lua"))()
+    success = true
+elseif placeId == 2345678901 then
+    gameName = "Game 6"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-6/main.lua"))()
+    success = true
+elseif placeId == 3456789012 then
+    gameName = "Game 7"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-7/main.lua"))()
+    success = true
+elseif placeId == 4567890123 then
+    gameName = "Game 8"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-8/main.lua"))()
+    success = true
+elseif placeId == 5678901234 then
+    gameName = "Game 9"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-9/main.lua"))()
+    success = true
+elseif placeId == 6789012345 then
+    gameName = "Game 10"
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/username/game-10/main.lua"))()
+    success = true
+end
+
+if success and gameName then
+    StarterGui:SetCore("SendNotification", {
+        Title = "Lexs Hub Loaded!",
+        Text = gameName .. " script loader!",
+        Duration = 6,
+        Icon = "rbxassetid://6023426926"
+    })
+else
+    StarterGui:SetCore("SendNotification", {
+        Title = "Lexs HUB",
+        Text = (gameName or "Game") .. " Not Found!",
+        Duration = 6,
+        Icon = "rbxassetid://6023426923"
+    })
+end
