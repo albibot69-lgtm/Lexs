4087b4e15949160a97c4f06c50c2f4793f63fdd9
LexsHubNew <albibot69@gmail.com>
2025-10-29 19:06:03 +0700

    Update premiummaclibUi.lua

--- Fish_It/premiummaclibUi.lua
+++ Fish_It/premiummaclibUi.lua
@@ -1,1 +1,17 @@
+local MacLib = loadstring(game:HttpGet("https://github.com/biggaboy212/Maclib/releases/latest/download/maclib.txt"))()
 
+local Window = MacLib:Window({
+    Title = "Kuzu Hub",
+    Subtitle = "Paid | V3.12",
+    Size = UDim2.fromOffset(650, 400),
+    DragStyle = 1,
+    DisabledWindowControls = {},
+    ShowUserInfo = true,
+    Keybind = Enum.KeyCode.RightControl,
+    AcrylicBlur = true,
+})
+
+local Tab1 = TabGroup:Tab({
+  Name <string>
+  Image <string> -- Image can be at maximum 16 pixels wide and 16 pixels tall.
+})
