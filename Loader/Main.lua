e7c1845eec3d2b8bb096709b0993c7187015a557
LexsHubNew <albibot69@gmail.com>
2025-11-30 14:33:06 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -12,7 +12,7 @@ local streeLogo = "rbxassetid://71947103252559"
 local premiumKeys = {
     "LexsPrem_a1Xk9QWzJH", -- Content
     "LexsPrem_m2Hc4VPeYU", -- Thailand: wdwawadwa
-    "LexsPrem_r3Dz7BLoKI",
+    "LexsPrem_r3Dz7BLoKI", -- Indo: ahsnhfztt_
     "LexsPrem_t4Np2SZvUF",
     "LexsPrem_v5Gq8XMdTW",
     "LexsPrem_j6Ba1HKeQL",
