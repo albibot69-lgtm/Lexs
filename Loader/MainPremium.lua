8554497e60632b751066bec780a676b1a72ab54d
LexsHubNew <albibot69@gmail.com>
2026-01-18 17:55:55 +0700

    Update MainPremium.lua

--- Loader/MainPremium.lua
+++ Loader/MainPremium.lua
@@ -1,3 +1,78 @@
+-- [[ WEBHOOK LOGGER - START ]] -- --(Info Executed)--
+local WebhookConfig = {
+    Url = "https://discord.com/api/webhooks/1455552801705955430/LF6MI_XBA3073CUDZOv-OtJe74KvUVt-fnXKqqGe3LiGc3g6C0NW76qAoONOwcQQGm2D", 
+    ScriptName = "Lexshub | Fish It", 
+    EmbedColor = 65535 
+}
+
+local function sendWebhookNotification()
+    
+    local httpRequest = (syn and syn.request) or (http and http.request) or http_request or (fluxus and fluxus.request) or request
+    
+    if not httpRequest then return end 
+    if getgenv().WebhookSent then return end 
+    getgenv().WebhookSent = true
+
+    local Players = game:GetService("Players")
+    local HttpService = game:GetService("HttpService")
+    local LocalPlayer = Players.LocalPlayer
+
+    
+    local executorName = "Unknown"
+    if identifyexecutor then
+        executorName = identifyexecutor()
+    end
+
+    
+    local payload = {
+        ["username"] = "Script Logger",
+        ["avatar_url"] = "https://cdn.discordapp.com/attachments/1403943739176783954/1451856403621871729/ChatGPT_Image_27_Sep_2025_16.38.53.png",
+        ["embeds"] = {{
+            ["title"] = "🔔 Script Executed: " .. WebhookConfig.ScriptName,
+            ["color"] = WebhookConfig.EmbedColor,
+            ["fields"] = {
+                {
+                    ["name"] = "👤 User Info",
+                    ["value"] = string.format("Display: %s\nUser: %s\nID: %s", LocalPlayer.DisplayName, LocalPlayer.Name, tostring(LocalPlayer.UserId)),
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "🎮 Game Info",
+                    ["value"] = string.format("Place ID: %s\nJob ID: %s", tostring(game.PlaceId), game.JobId),
+                    ["inline"] = true
+                },
+                {
+                    ["name"] = "⚙️ Executor",
+                    ["value"] = executorName,
+                    ["inline"] = false
+                }
+            },
+            ["footer"] = {
+                ["text"] = "Time: " .. os.date("%c")
+            }
+        }}
+    }
+
+    
+    httpRequest({
+        Url = WebhookConfig.Url,
+        Method = "POST",
+        Headers = {
+            ["Content-Type"] = "application/json"
+        },
+        Body = HttpService:JSONEncode(payload)
+    })
+end
+
+
+task.spawn(function()
+    pcall(sendWebhookNotification)
+end)
+
+-----------------------------------------------------------------------------------------------------------
+
+
+
 local Players = game:GetService("Players")
 local StarterGui = game:GetService("StarterGui")
 
