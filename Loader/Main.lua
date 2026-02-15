bbaa62f6dc70ee3b82cba08685b52bd15f5572a8
LexsHubNew <albibot69@gmail.com>
2026-02-15 16:46:33 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,80 +1,13 @@
-local StarterGui = game:GetService("StarterGui")
-local TweenService = game:GetService("TweenService")
-local CoreGui = game:GetService("CoreGui")
-local placeId = game.PlaceId
-
-_G.scripts_key = _G.scripts_key or scripts_key or "FREE_USER"
-local Logo = "rbxassetid://122683047852451"
-
-local premiumKeys = {
-    "hRCWybDuIIxXeREImBbvjsEueohPzTfX",
+local games = {
+    [121864768012064] = "https://raw.githubusercontent.com/MajestySkie/Chloe-X/refs/heads/main/Games/FishIt.lua",
+    [131623223084840] = "https://raw.githubusercontent.com/MajestySkie/Chloe-X/refs/heads/main/Games/Abyss.lua",
 }
 
-local function isPremiumKey(key)
-    for _, k in ipairs(premiumKeys) do
-        if key == k then
-            return true
-        end
-    end
-    return false
-end
+local currentID = game.PlaceId
+local scriptURL = games[currentID]
 
-local userType = isPremiumKey(_G.scripts_key) and "Premium" or "Freemium"
-
-local gameScripts = {
-    [131623223084840] = {
-        name = "Escape Tsunami For Brainrot",
-        free = "https://raw.githubusercontent.com/albibot69-lgtm/Lexs/refs/heads/main/Loader/LoadingScreenFreemium.lua",
-        premium = "https://raw.githubusercontent.com/-a"
-    },
-    [121864768012064] = {
-        name = "Fish It",
-        free = "https://raw.githubusercontent.com/-",
-        premium = "https://raw.githubusercontent.com/-"
-    }
-}
-
-local gameData = gameScripts[placeId]
-local gameName = gameData and gameData.name or "Unknown Game"
-
-StarterGui:SetCore("SendNotification", {
-    Title = "STREE HUB",
-    Text = "Detected game: " .. gameName,
-    Icon = Logo,
-    Duration = 3
-})
-StarterGui:SetCore("SendNotification", {
-    Title = "STREE HUB",
-    Text = "User Type: " .. userType,
-    Icon = Logo,
-    Duration = 3
-})
-
-task.wait(2)
-
-if gameData then
-    if userType == "Premium" then
-        StarterGui:SetCore("SendNotification", {
-            Title = "STREE HUB",
-            Text = "Loading Premium version for " .. gameName .. "...",
-            Icon = Logo,
-            Duration = 3
-        })
-        loadstring(game:HttpGet(gameData.premium))()
-    else
-        StarterGui:SetCore("SendNotification", {
-            Title = "STREE HUB",
-            Text = "Loading Freemium version for " .. gameName .. "...",
-            Icon = Logo,
-            Duration = 3
-        })
-        loadstring(game:HttpGet(gameData.free))()
-    end
+if scriptURL then
+    loadstring(game:HttpGet(scriptURL))()
 else
-    StarterGui:SetCore("SendNotification", {
-        Title = "STREE HUB",
-        Text = "Game not supported!",
-        Icon = Logo,
-        Duration = 4
-    })
+    game.Players.LocalPlayer:Kick("Yo! This game ain't on the list.\nCheck the Discord for whitelisted games, homie.")
 end
