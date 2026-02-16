95911a9d6fe0fac578734564ee82ce2dc8ee33bb
LexsHubNew <albibot69@gmail.com>
2026-02-16 14:30:32 +0700

    Update Main.lua

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,6 +1,7 @@
 local games = {
     [121864768012064] = "https://raw.githubusercontent.com/----------",
     [131623223084840] = "https://raw.githubusercontent.com/albibot69-lgtm/Lexs/refs/heads/main/ETFB/Freemium.lua",
+    [16981421605] = "https://raw.githubusercontent.com/albibot69-lgtm/Lexs/refs/heads/main/ETFB/Freemium.lua",
 }
 
 local currentID = game.PlaceId
